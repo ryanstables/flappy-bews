fcbe7d9cbe988cd6cba3e9e564d04d858d530359
Ryan Stables <ryan.stables@roli.com>
2023-04-21 16:05:18 +0100

    Colour works better

--- Assets/Hurdle.cs
+++ Assets/Hurdle.cs
@@ -11,18 +11,33 @@ public class Hurdle : MonoBehaviour
     public float speedMultiplier = 5.0f;
     private float deletePos = -40;
 
+    private Color[] colours = {
+      new Color(194f/255f, 0f/255f, 100f/255f),
+      new Color(208f/255f, 64f/255f, 88f/255f),
+      new Color(222f/255f, 128f/255f, 75f/255f),
+      new Color(233f/255f, 173f/255f, 2f/255f),
+      new Color(223f/255f, 204f/255f, 6f/255f),
+      new Color(134f/255f, 192f/255f, 59f/255f),
+      new Color(18f/255f, 128f/255f, 68f/255f),
+      new Color(43f/255f, 134f/255f, 162f/255f),
+      new Color(68f/255f, 140f/255f,  255f/255f),
+      new Color(65f/255f, 72f/255f, 220f/255f),
+      new Color(94f/255f, 78f/255f, 211f/255f),
+      new Color(155f/255f, 0f/255f, 224f/255f)
+    };
+
     Vector3 getSpeed() {
       // convert the tempo of the song to another coeff here...
       return (Vector3.left * speedMultiplier) * Time.deltaTime;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-      
+      Color pipeColour = colours[Random.Range(0, colours.Length)];
+      bottomPipe.GetComponent<Pipe>().setColor(pipeColour);
+      topPipe.GetComponent<Pipe>().setColor(pipeColour);
     }
 
-    // Update is called once per frame
     void Update()
     {
       transform.position += getSpeed();
